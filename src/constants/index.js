abf28f3329e91a9c256dcf4decda6f17684e1340
AC12arpitChauhan <arpitchauhanac07@gmail.com>
2024-10-14 23:10:54 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,288 @@
+export const navLinks = [
+    {
+      id: 1,
+      name: 'Home',
+      href: '#home',
+    },
+    {
+      id: 2,
+      name: 'About',
+      href: '#about',
+    },
+    {
+      id: 3,
+      name: 'Work',
+      href: '#work',
+    },
+    {
+      id: 4,
+      name: 'Contact',
+      href: '#contact',
+    },
+  ];
+  
+  export const clientReviews = [
+    {
+      id: 1,
+      name: 'Mahak Chauhan',
+      position: 'Marketing Director at GreenLeaf',
+      img: 'assets/review1.png',
+      review:
+        'Working with Arpit was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.',
+    },
+    {
+      id: 2,
+      name: 'Mr. Aayush Singh',
+      position: 'SDE at Mastercard',
+      img: 'assets/review2.png',
+      review:
+        'Arpit’s expertise in web development and problem-solving is commendable. His work on our class project demonstrated his deep understanding of both frontend and backend technologies. I highly recommend him for any development role.',
+    },
+    {
+      id: 3,
+      name: 'Dr. Gaurav Kumar Bharti',
+      position: 'Assistant Professor of ECE, IIIT Bhopal',
+      img: 'assets/review3.png',
+      review:
+        'Arpit has shown remarkable dedication to his studies and projects. He consistently exceeds expectations in both technical proficiency and creative problem-solving. His contributions to class discussions and group projects are always insightful and valuable.',
+    },
+    {
+      id: 4,
+      name: 'Hemang Gupta',
+      position: 'Lecturer in Web Development, Ehwa University',
+      img: 'assets/review4.png',
+      review:
+        'Arpit was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend backend dev are top-notch.',
+    },
+  ];
+  
+  export const myProjects = [
+    {
+      title: 'Tomato - Food Delivery Platform',
+      desc: 'Tomato is a game-changing food delivery platform that connects users with a wide variety of restaurants and cuisines. Offering seamless browsing, ordering, and delivery, it empowers customers to enjoy their favorite meals right from the comfort of their homes.',
+      subdesc:
+        'Built as a cutting-edge platform with React.js, Node.js, MongoDB, and Tailwind CSS, Tomato is optimized for speed, user experience, and scalability, ensuring quick and reliable service across devices.',
+      href: 'https://food-del-frontend-yz04.onrender.com',
+      texture: '/textures/project/tomato.mp4',
+      logo: '/assets/project-logo1.png',
+      logoStyle: {
+        backgroundColor: '#2A1816',
+        border: '0.2px solid #36201D',
+        boxShadow: '0px 0px 60px 0px #AA3C304D',
+      },
+      spotlight: '/assets/spotlight1.png',
+      tags: [
+        {
+          id: 1,
+          name: 'React.js',
+          path: '/assets/react.svg',
+        },
+        {
+          id: 2,
+          name: 'Node.JS',
+          path: 'assets/node.png',
+        },
+        {
+          id: 3,
+          name: 'JavaScript',
+          path: '/assets/JavaScript-logo.png',
+        },
+        {
+          id: 4,
+          name: 'MongoDB',
+          path: '/assets/mongodb.svg',
+        },
+      ],
+    },
+    {
+      title: 'Shopper - E-commerce Shopping Platform',
+      desc: 'Shopper is a modern e-commerce platform that redefines online clothes shopping. With a vast collection of trendy apparel, it offers customers a seamless shopping experience from product discovery to checkout.',
+      subdesc:
+        'Powered by the MERN stack, Integrated with secure payment gateways and optimized for responsiveness, it ensures a secure shopping experience across all devices.',
+      href: 'https://e-com-frontend-0dyw.onrender.com',
+      texture: '/textures/project/shopper.mp4',
+      logo: '/assets/shopper.png',
+      logoStyle: {
+        backgroundColor: '#13202F',
+        border: '0.2px solid #17293E',
+        boxShadow: '0px 0px 60px 0px #2F6DB54D',
+      },
+      spotlight: '/assets/spotlight2.png',
+      tags: [
+        {
+          id: 1,
+          name: 'React.js',
+          path: '/assets/mongodb.svg',
+        },
+        {
+          id: 2,
+          name: 'TailwindCSS',
+          path: 'assets/JavaScript-logo.png',
+        },
+        {
+          id: 3,
+          name: 'TypeScript',
+          path: '/assets/react.svg',
+        },
+        {
+          id: 4,
+          name: 'Framer Motion',
+          path: '/assets/node.png',
+        },
+      ],
+    },
+    {
+      title: "Client's Portfolio - A Portfolio",
+      desc: 'This portfolio is a sleek portfolio platform that showcases projects and skills with an interactive, modern design. It provides an engaging way to highlight achievements and work.',
+      subdesc:
+        'Built using React, Next.js, Three.js, Tailwind CSS, Aceternity UI, and Framer Motion, it ensures smooth animations, responsiveness, and high performance.',
+      href: 'https://github.com/AC12arpitChauhan/portfolios',
+      texture: '/textures/project/portfolio.mp4',
+      logo: '/assets/three.jpeg',
+      logoStyle: {
+        backgroundColor: '#60f5a1',
+        background:
+          'linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)',
+        border: '0.2px solid rgba(208, 213, 221, 1)',
+        boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
+      },
+      spotlight: '/assets/spotlight3.png',
+      tags: [
+        {
+          id: 1,
+          name: 'React.js',
+          path: '/assets/react.svg',
+        },
+        {
+          id: 2,
+          name: 'TailwindCSS',
+          path: 'assets/tailwindcss.png',
+        },
+        {
+          id: 3,
+          name: 'TypeScript',
+          path: '/assets/typescript.png',
+        },
+        {
+          id: 4,
+          name: 'Framer Motion',
+          path: '/assets/framer.png',
+        },
+      ],
+    },
+    // {
+    //   title: 'Horizon - Online Banking Platform',
+    //   desc: 'Horizon is a comprehensive online banking platform that offers users a centralized finance management dashboard. It allows users to connect multiple bank accounts, monitor real-time transactions, and seamlessly transfer money to other users.',
+    //   subdesc:
+    //     'Built with Next.js 14 Appwrite, Dwolla and Plaid, Horizon ensures a smooth and secure banking experience, tailored to meet the needs of modern consumers.',
+    //   href: 'https://www.youtube.com/watch?v=PuOVqP_cjkE',
+    //   texture: '/textures/project/project4.mp4',
+    //   logo: '/assets/project-logo4.png',
+    //   logoStyle: {
+    //     backgroundColor: '#0E1F38',
+    //     border: '0.2px solid #0E2D58',
+    //     boxShadow: '0px 0px 60px 0px #2F67B64D',
+    //   },
+    //   spotlight: '/assets/spotlight4.png',
+    //   tags: [
+    //     {
+    //       id: 1,
+    //       name: 'React.js',
+    //       path: '/assets/react.svg',
+    //     },
+    //     {
+    //       id: 2,
+    //       name: 'TailwindCSS',
+    //       path: 'assets/tailwindcss.png',
+    //     },
+    //     {
+    //       id: 3,
+    //       name: 'TypeScript',
+    //       path: '/assets/typescript.png',
+    //     },
+    //     {
+    //       id: 4,
+    //       name: 'Framer Motion',
+    //       path: '/assets/framer.png',
+    //     },
+    //   ],
+    // },
+    // {
+    //   title: 'Imaginify - AI Photo Manipulation App',
+    //   desc: 'Imaginify is a groundbreaking Software-as-a-Service application that empowers users to create stunning photo manipulations using AI technology. With features like AI-driven image editing, a payments system, and a credits-based model.',
+    //   subdesc:
+    //     'Built with Next.js 14, Cloudinary AI, Clerk, and Stripe, Imaginify combines cutting-edge technology with a user-centric approach. It can be turned into a side income or even a full-fledged business.',
+    //   href: 'https://www.youtube.com/watch?v=Ahwoks_dawU',
+    //   texture: '/textures/project/project5.mp4',
+    //   logo: '/assets/project-logo5.png',
+    //   logoStyle: {
+    //     backgroundColor: '#1C1A43',
+    //     border: '0.2px solid #252262',
+    //     boxShadow: '0px 0px 60px 0px #635BFF4D',
+    //   },
+    //   spotlight: '/assets/spotlight5.png',
+    //   tags: [
+    //     {
+    //       id: 1,
+    //       name: 'React.js',
+    //       path: '/assets/react.svg',
+    //     },
+    //     {
+    //       id: 2,
+    //       name: 'TailwindCSS',
+    //       path: 'assets/tailwindcss.png',
+    //     },
+    //     {
+    //       id: 3,
+    //       name: 'TypeScript',
+    //       path: '/assets/typescript.png',
+    //     },
+    //     {
+    //       id: 4,
+    //       name: 'Framer Motion',
+    //       path: '/assets/framer.png',
+    //     },
+    //   ],
+    // },
+  ];
+  
+  export const calculateSizes = (isSmall, isMobile, isTablet) => {
+    return {
+      deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+      deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
+      cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
+      reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
+      ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
+      targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
+    };
+  };
+  
+  export const workExperiences = [
+    {
+      id: 1,
+      name: 'IEEE IIIT Bhopal Student Branch',
+      pos: 'Scocial Media Co-ordinator',
+      duration: '2023',
+      title: "I have successfully led and engaged teams as an IEEE Social Media Manager and as admin of IIIT Bhopal's social media pages.",
+      icon: '/assets/ieee.webp',
+      animation: 'victory',
+    },
+    {
+      id: 3,
+      name: 'Code-chef',
+      pos: '2-star-Coder',
+      duration: '2024',
+      title: "I have showcased my problem-solving abilities and expertise in competitive programming. This achievement reflects my dedication to improving algorithmic thinking and tackling complex challenges efficiently.",
+      icon: '/assets/codechef.jpg',
+      animation: 'clapping',
+    },
+    {
+      id: 2,
+      name: 'Udemy',
+      pos: 'Completed Full-Stack course',
+      duration: '2023 - 2024',
+      title: "I completed a comprehensive full-stack web development journey through a Udemy course, gaining hands-on experience with both frontend and backend technologies. This solid foundation has empowered me to build dynamic, scalable web applications.",
+      icon: '/assets/udemy.jpg',
+      animation: 'salute',
+    },
+  ];
